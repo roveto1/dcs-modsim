5c49371b642106a84c3525709c25eff2b359fb35
roveto <robertovanneto@gmail.com>
2025-07-10 09:54:39 -0300

    teste

--- docker/generate_gui_bin.sh
+++ docker/generate_gui_bin.sh
@@ -12,7 +12,7 @@ if ! grep -E "$PATTERN" "$FILE" > /dev/null; then
     exit 1
 fi
 
-# Cria o binário da GUI do GenESyS
+# Cria o binário da GUI do GenESyS TESTE
 cd $GENESYS_PROJECT/$GENESYS_QT_PROJECT_SUBPATH
 qmake6 $GENESYS_QT_PROJECT
 cd $GENESYS_PROJECT/$GENESYS_GUI_SUBPATH
